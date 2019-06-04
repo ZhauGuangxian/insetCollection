da0cc3dd3dfdb946f7c6fd2409b86dc8acfc4637
宅光咸 <1054257376@qq.com>
2019-06-04 20:30:54 +0800

    后台使用async await，加深理解

--- server/routers/insets.js
+++ server/routers/insets.js
@@ -8,7 +8,7 @@ const responseClient = require('../util/util.js').responseClient;
 const ObjectId = require('mongodb').ObjectID;
 
 router.post('/getNewestInsets',(req,res)=>{
-    
+    let id = req.decoded['_id'];
     let limit=6;
     let skip=0
     
@@ -16,12 +16,35 @@ router.post('/getNewestInsets',(req,res)=>{
         
         skip = (req.pageNum-1) * 6
     }
-    MongoClient.connect(dbUrl,{useNewUrlParser:true},(err,db)=>{
+    MongoClient.connect(dbUrl,{useNewUrlParser:true},async (err,db)=>{
         if(err){
             throw err
         }
         let dbi = db.db("InsetCollection");
-        dbi.collection('insets').find({},{limit,skip}).toArray((error,result)=>{
+        let allNewest = null;
+        let getAllNew = async ()=>{
+            
+            let result1 = await new Promise((resolve,reject)=>{
+                dbi.collection('insets').find({},{limit,skip}).toArray((error,result)=>{
+                    db.close();
+                 
+                    if(result){
+                        resolve(result);
+                        //responseClient(res,200,1,'success',result)
+                    }else{
+                        //responseClient(res,400,1,'faild',{})
+                        reject(error)
+                    }
+                })
+            })
+            allNewest = result1;
+        }
+        
+        await getAllNew();
+
+        
+        responseClient(res,200,1,'success',allNewest)
+        /*dbi.collection('insets').find({},{limit,skip}).toArray((error,result)=>{
             db.close();
             if(error){
                 throw error
@@ -32,22 +55,51 @@ router.post('/getNewestInsets',(req,res)=>{
             }else{
                 responseClient(res,400,1,'faild',{})
             }
-        })
+        })*/
     })
 })
 router.all('/getMyById',(req,res)=>{
-    let id = "5cbebeb8e472d7e69d21eaf5";;
+    
+    let id = req.decoded['_id'];
     MongoClient.connect(dbUrl,{useNewUrlParser:true},(err,db)=>{
         let dbi = db.db("InsetCollection");
+        let getsubscribeIds =async ()=>{
+            //await还挺好用的
+            let ids = await  dbi.collection('users').findOne({_id:ObjectId(id)},{projection:{subscribeuserIds:1}})
+            let idsCollect = ids.subscribeuserIds || [];
+            if(idsCollect.length > 0){
+                idsCollect.forEach(e=>{
+                    e = ObjectId(e);
+                })
+            }
+            dbi.collection('insets').find({author:{$in:idsCollect}}).toArray((error,result)=>{
+                if(result){
+                   
 
-        dbi.collection('users').findOne({_id:ObjectId(id)},{},(error,result)=>{
-            console.log(result)
-            db.close();
-            responseClient(res,200,1,'success',result)
-        })
+                    responseClient(res,200,1,'success',result);
+                }else{
+                    responseClient(res,400,1,'failed',error);
+                }
+                db.close();
+            })
+            
+
+
+            /**
+             * ,(error,result)=>{
+                if(result){
+                    resolve(result)
+                }else{
+                    reject(error)
+                }  
+            }
+             */
+        }
+        
+        getsubscribeIds();
     })
 })
-router.post('/getHomeInsets',(req,res)=>{
+/*router.post('/getHomeInsets',(req,res)=>{
     //判断一下是否登录先
     let id = "5cbebeb8e472d7e69d21eaf5";
     let _id =  new ObjectId(id);
@@ -67,6 +119,6 @@ router.post('/getHomeInsets',(req,res)=>{
 
         responseClient(res,200,1,'success',subscribeids)
     })
-})
+})*/
 
 module.exports = router;
