c3b28ac7c6f4923389603d187148ffd40d2d69ed
宅光咸 <1054257376@qq.com>
2019-06-13 20:32:31 +0800

    update

--- src/views/lab/children/canvas/inputBloom/inputBloom.vue
+++ src/views/lab/children/canvas/inputBloom/inputBloom.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="inputBloom">
         <p>在输入框里输入</p>
-        
+
     </div>
 </template>
 
 <script>
 export default {
-    
+    name:"InputBloom",
+    data(){
+        return{
+            
+        }
+    }
 }
 </script>
 
